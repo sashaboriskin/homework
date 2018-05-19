2fadf88a366cd35d2f71052a760bba559ba6cc8f
sashaboriskin <32432478+sashaboriskin@users.noreply.github.com>
2018-05-19 13:42:20 +0300

    Update geometry_classes.py

--- geometry_classes.py
+++ geometry_classes.py
@@ -1,28 +1,68 @@
-'''base = 5
-heigth = 3
-str = ''
+#class geofigur:
+    #def __init__(self, base, height):
+        #self.height = height
+        #self.base = base
 
-for i in range(base):
-    str = str + ' ' + '-'
-    if i == 0:
-        for i in range(heigth):
+class trapeze:
+  def __init__(self, base, base2, height):
+    self.base = base
+    self.base2 = base2
+    self.height = height
 
-            print('|' )
 
-print(str)
+class triangle:
+  def __init__(self, base, height):
+    self.base = base
+    self.height = height  
+
+  def paint(self):
+    for i in range(self.height): 
+      b = 1+ (i/self.height)*self.base 
+      str = '' 
+      for i in range(int(b)): 
+        str = str + '*' 
+      print(str)
+    
+class rectangle:
+  def __init__(self, base, height):
+    self.base = base
+    self.height = height
+  
+  def paint(self):
+    for i in range(self.height):
+      str = ''
+      for i in range(self.base):
+        str = str + ' ' + "*"
+      print(str)
+
+
+
+#fff = triangle(5, 4)
+#fff.paint()
+
+ddd = rectangle(5, 4)
+ddd.paint()
+
+
 '''
-class geofigur:
-    def __init__(self, base, height):
-        self.height = height
-        self.base = base
+A = 4
+B = 5
 
-class trapeze(geofigur):
-
-class triangle(geofigur):
-
-class rectangle(geofigur):
+for i in range(A):
+  str = ''
+  for i in range(B):
+    str = str + ' ' + "*"
+  print(str)
 
 
+A = 5 
+B = 4 
 
 
-
+for i in range(A): 
+  b = 1+ (i/A)*B 
+  str = '' 
+  for i in range(int(b)): 
+    str = str + '*' 
+  print(str)  
+  '''
