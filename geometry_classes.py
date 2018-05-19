e769afb38b228137b181350ff06a50733afcad51
sashaboriskin <32432478+sashaboriskin@users.noreply.github.com>
2018-05-19 14:28:43 +0300

    Update geometry_classes.py

--- geometry_classes.py
+++ geometry_classes.py
@@ -1,33 +1,23 @@
-#class geofigur:
-    #def __init__(self, base, height):
-        #self.height = height
-        #self.base = base
+class geofigur:
+    def __init__(self, base, height):
+        self.height = height
+        self.base = base
 
-class trapeze:
-  def __init__(self, base, base2, height):
-    self.base = base
+class trapeze(geofigur):
+  def second_base(self, base2):
     self.base2 = base2
-    self.height = height
+  
 
-
-class triangle:
-  def __init__(self, base, height):
-    self.base = base
-    self.height = height  
-
+class triangle(geofigur):
   def paint(self):
     for i in range(self.height): 
-      b = 1+ (i/self.height)*self.base 
+      b = 1 + (i/self.height)*self.base 
       str = '' 
       for i in range(int(b)): 
         str = str + '*' 
       print(str)
     
-class rectangle:
-  def __init__(self, base, height):
-    self.base = base
-    self.height = height
-  
+class rectangle(geofigur):  
   def paint(self):
     for i in range(self.height):
       str = ''
@@ -36,33 +26,8 @@ class rectangle:
       print(str)
 
 
-
-#fff = triangle(5, 4)
-#fff.paint()
+fff = triangle(4, 5)
+fff.paint()
 
 ddd = rectangle(5, 4)
 ddd.paint()
-
-
-'''
-A = 4
-B = 5
-
-for i in range(A):
-  str = ''
-  for i in range(B):
-    str = str + ' ' + "*"
-  print(str)
-
-
-A = 5 
-B = 4 
-
-
-for i in range(A): 
-  b = 1+ (i/A)*B 
-  str = '' 
-  for i in range(int(b)): 
-    str = str + '*' 
-  print(str)  
-  '''
